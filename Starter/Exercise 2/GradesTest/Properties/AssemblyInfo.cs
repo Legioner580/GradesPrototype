516e93d88111a68a3c05cd37fd93f066db18525b
Administrator <Administrator@WIN->
2022-10-21 07:34:40 -0700

    Add project files.

---
+++ Starter/Exercise 2/GradesTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GradesTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GradesTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("03efaf31-e78a-4d94-af0c-fcf551379fb4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Starter/Exercise 4/GradesPrototype/Controls/AssignStudentDialog.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using GradesPrototype.Data;
+using GradesPrototype.Services;
+
+namespace GradesPrototype.Controls
+{
+    /// <summary>
+    /// Interaction logic for AssignStudentDialog.xaml
+    /// </summary>
+    public partial class AssignStudentDialog : Window
+    {
+        public AssignStudentDialog()
+        {
+            InitializeComponent();
+        }
+
+        // TODO: Exercise 4: Task 3b: Refresh the display of unassigned students
+        private void Refresh()
+        {
+            
+        }
+
+        private void AssignStudentDialog_Loaded(object sender, RoutedEventArgs e)
+        {
+            Refresh();
+        }
+
+        // TODO: Exercise 4: Task 3a: Enroll a student in the teacher's class
+
+        private void Student_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Determine which student the user clicked
+                // the StudentID is held in the Tag property of the Button that the user    clicked
+                Button studentClicked = sender as Button;
+                int studentID = (int)studentClicked.Tag;
+
+                // Find this student in the Students collection
+                Student student = (from s in DataSource.Students
+                                   where s.StudentID == studentID
+                                   select s).First();
+
+                // Prompt the user to confirm that they wish to add this student to their class
+                string message = String.Format("Add {0} {1} to your class?", student.FirstName, student.LastName);
+                MessageBoxResult reply = MessageBox.Show(message, "Confirm", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                // If the user confirms, add the student to their class
+                if (reply == MessageBoxResult.Yes)
+                {
+                    // Get the ID of the currently logged-on teacher
+                    int teacherID = SessionContext.CurrentTeacher.TeacherID;
+
+                    // Assign the student to this teacher’s class
+                    SessionContext.CurrentTeacher.EnrollInClass(student);
+
+                    // Refresh the display – the new assigned student should disappear from     the list of unassigned students
+                    Refresh();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error enrolling student", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            // Close the dialog box
+            this.Close();
+        }
+    }
+}
